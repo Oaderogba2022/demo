3bd1467f0c9617ff1b9f881f3c26a0c9600cb48b
Olamide Aderogba <G00401971@atu.ie>
2023-09-30 16:42:27 +0100

    yy

--- src/main/java/ie/atu/demo/Demo.java
+++ src/main/java/ie/atu/demo/Demo.java
@@ -9,4 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Demo {
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello, World!";
+
+    }
+
+
 }
